2b66233e3d41e789de34c5bd6c306e18a033dd2f
Andre Jaenisch <andre.jaenisch@openmailbox.org>
2014-09-11 23:26:03 +0200

    Inserted Button to translate names into Japanese.

--- SuK.js
+++ SuK.js
@@ -5,7 +5,7 @@
 // @match       http://schlacht-um-kyoto.de/*
 // @match       http://www.schlacht-um-kyoto.de/*
 // @require     https://code.jquery.com/jquery-1.6.2.min.js
-// @version     0.0.1
+// @version     0.0.3
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ==/UserScript==
@@ -19,24 +19,43 @@ if (window.top === window.self) {
   japanBtn.type = 'button';
   japanBtn.classList.add('button');
   japanBtn.classList.add('small');
-  japanBtn.classList.add('pressed');
   japanBtn.id = 'japanBtn';
   japanBtn.title = 'Sprache wechseln';
   japanBtn.value = 'Kanji';
   tdContainer.appendChild(japanBtn)
   headline[0].insertBefore(tdContainer, headline[0].children[2]);
-  headline[1].children[0].colspan = '6';
-  headline[2].children[0].colspan = '4';
+  japanBtn = null;
+  japanBtn = document.querySelector('#japanBtn');
+  japanBtn.addEventListener('click', function() {
+      this.classList.toggle('pressed');
+      translate();
+  }, false);
+  console.log(japanBtn);
+  // Adjusting the table
+  var colgrp = document.querySelector('#chatForm colgroup');
+  var japanCol = document.createElement('col');
+  japanCol.width = 70;
+  colgrp.insertBefore(japanCol, colgrp.children[2]);
+  headline[1].children[0].setAttribute('colspan',6);
+  headline[2].children[0].setAttribute('colspan',4);
+  document.querySelector('#chatInput').width = '490px';
       
-  var hepburn = require('hepburn');
-  var chatMessage = document.querySelectorAll('.chatMessage');
-  var authorTime, authorTimePair, author, time;
-  for (var i = 0; i < chatMessage.length; i++) {
-      authorTime = chatMessage[i].children[0];
-      authorTimePair = authorTime.innerHTML.split('(');
-      author = authorTimePair[0];
-      time = authorTimePair[1].split(')')[0];
-      authorTime.innerHTML = hepburn.toHiragana(author) + '(' + time + ')';
+  function translate() {
+      var hepburn = require('hepburn');
+      var chatMessage = document.querySelectorAll('.chatMessage');
+      if (japanBtn.classList.contains('pressed')) {
+          var authorTime, authorTimePair, author, time;
+          for (var i = 0; i < chatMessage.length; i++) {
+              authorTime = chatMessage[i].children[0];
+              authorTimePair = authorTime.innerHTML.split('(');
+              author = authorTimePair[0];
+              if (author !== 'Du schreibst ') {
+                  author = hepburn.toHiragana(author);
+              }
+              time = authorTimePair[1].split(')')[0];
+              authorTime.innerHTML = author + '(' + time + ')';
+          }
+      }
   }
 
   // settingsObject by GreaseSpot, slightly modified
